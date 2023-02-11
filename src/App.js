7426b156b68b8d96ac81512afc807e7c99b88885
Sergii <wheelyhog@gmail.com>
2023-02-11 16:23:28 +0100

    43 lesson -Cannot read properties of undefined (reading 'map')     at MyPosts (MyPosts.jsx:7:1)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+// import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import { Route, Routes } from 'react-router-dom';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 
 const App = (props) => {
@@ -15,8 +16,8 @@ const App = (props) => {
       <Navbar />
       <div className="app-wrapper-content">
         <Routes>
-          <Route path="/dialogs" element={<Dialogs store={props.store} />} />
-          <Route path="/profile" element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
+          <Route path="/dialogs" element={<DialogsContainer store={props.store} />} />
+          <Route path="/profile" element={<Profile store={props.store} />} />
           <Route path="/news" element={<News />} />
         </Routes>
       </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
-import { addPostActionCreator, updateNewPostTextActionCreator } from "../../redux/profile-reducer";
 
 const MyPosts = (props) => {
 
@@ -9,14 +8,13 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
     }
 
     return <div className={s.postsBlock}>
@@ -24,7 +22,7 @@ const MyPosts = (props) => {
         <div>
             <textarea onChange={onPostChange} ref={newPostElement} className={s.posts__textarea} value={props.newPostText} />
             <div>
-                <button onClick={addPost} className={s.add__post}>Add Post</button>
+                <button onClick={onAddPost} className={s.add__post}>Add Post</button>
             </div>
         </div>
         <div className={s.posts}>
