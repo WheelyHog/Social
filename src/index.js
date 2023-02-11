16daf53aecd673fd31ca2eacd3cb7eb71533055b
Sergii <wheelyhog@gmail.com>
2023-02-11 05:51:52 +0100

    39 lesson after correction a  mistake in import ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { addPost, subscribe, updateNewPostText } from './components/redux/state';
 import './index.css';
 import store from './components/redux/state';
-import { ReactDOM } from "react";
+import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
