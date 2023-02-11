8d78c73ea40ec13fe8295bdabeff23fb5d602483
Sergii <wheelyhog@gmail.com>
2023-02-11 08:12:19 +0100

    40 lesson +

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import { Route, Routes } from 'react-router-dom';
-import { addPost } from './components/redux/state';
+
 
 const App = (props) => {
 
@@ -16,7 +16,7 @@ const App = (props) => {
       <Navbar />
       <div className="app-wrapper-content">
         <Routes>
-          <Route path="/dialogs" element={<Dialogs state={props.state.dialogsPage} />} />
+          <Route path="/dialogs" element={<Dialogs store={props.store} />} />
           <Route path="/profile" element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
           <Route path="/news" element={<News />} />
         </Routes>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import { sendMessageCreator, updateNewMessageBodyCreator } from "../redux/state";
 
 const Dialogs = (props) => {
 
-    let dialogsElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />)
-    let messagesElements = props.state.messages.map(message => <Message message={message.message} />)
+    let state = props.store.getState().dialogsPage;
+
+    let dialogsElements = state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />)
+    let messagesElements = state.messages.map(message => <Message message={message.message} />)
+    let newMessageBody = state.newMessageBody;
+
+    let onSensMessageClick = () => {
+        props.store.dispatch(sendMessageCreator())
+    }
+    let onNewMessageChange = (e) => {
+        let body = e.target.value;
+        props.store.dispatch(updateNewMessageBodyCreator(body));
+    }
 
     return (
         <div className={s.dialogs}>
@@ -14,7 +26,13 @@ const Dialogs = (props) => {
                 {dialogsElements}
             </div>
             <div className={s.messages}>
-                {messagesElements}
+                <div>
+                    {messagesElements}
+                </div>
+                <div>
+                    <div><textarea value={newMessageBody} onChange={onNewMessageChange} placeholder="Enter your message" /></div>
+                    <div><button onClick={onSensMessageClick}>Send</button></div>
+                </div>
             </div>
         </div>
     )
