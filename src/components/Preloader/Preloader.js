69d7783d8ef0b21d63f2ce36609a67eeb6937fb1
Sergii <wheelyhog@gmail.com>
2023-03-07 21:31:22 +0100

    lesson58+

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from '../../Spinner-3.gif'
+let Preloader = (props) => {
+    return <div style={{background: 'white'}}>
+        <img src={preloader}/>
+    </div>
+}
+
+export default Preloader
